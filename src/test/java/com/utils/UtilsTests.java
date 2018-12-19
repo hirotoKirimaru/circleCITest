39d3a859adb2dc050addf6428a5c76036640f6ec
nainaistar <nainaistar@gmail.com>
2018-12-19 23:49:46 +0900

    test:

--- src/test/java/com/utils/UtilsTests.java
+++ src/test/java/com/utils/UtilsTests.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.form.UserForm;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -71,4 +74,55 @@ public class UtilsTests {
 		assertEquals(actual , 200);
 	}
 	
+	
+	
+	/**
+	  * 複数の特定のカラムが重複していた場合、重複を削除する処理をテストする。
+	  * なお、今回はUserIdとVersionの重複しない場合として処理する。
+	 */
+	@Test
+	public void tekitou() {
+		List<UserForm> a = new ArrayList<>();
+		List<UserForm> b = new ArrayList<>();
+		UserForm tmpForm;
+		
+		tmpForm = new UserForm();
+		tmpForm.setUserId(100);
+		tmpForm.setVersion(1);
+		a.add(tmpForm);
+		
+		tmpForm = new UserForm();
+		tmpForm.setUserId(200);
+		tmpForm.setVersion(1);
+		a.add(tmpForm);
+		
+		tmpForm = new UserForm();
+		tmpForm.setUserId(300);
+		tmpForm.setVersion(1);
+		a.add(tmpForm);
+		
+		tmpForm = new UserForm();
+		tmpForm.setUserId(100);
+		tmpForm.setVersion(1);
+		b.add(tmpForm);
+		
+		tmpForm = new UserForm();
+		tmpForm.setUserId(400);
+		tmpForm.setVersion(1);
+		b.add(tmpForm);
+		
+		b.forEach(bx -> {
+			boolean flg = a.stream().filter(ax -> Objects.equals(bx.getUserId(), ax.getUserId())).anyMatch(a2 -> Objects.equals(a2.getVersion(), bx.getVersion()));
+			if (!flg) a.add(bx);
+		});
+		
+		a.forEach(ax -> {
+			System.out.println(ax.getUserId());
+			System.out.println(ax.getVersion());
+		});
+		
+		
+//		a.stream().filter(e -> Objects.equals(e.getUserId())
+		
+	}
 }
