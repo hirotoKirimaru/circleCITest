b050a1dfe7b380646ab678b89f48f25d839a7bc1
nainaistar <nainaistar@gmail.com>
2019-01-12 23:33:45 +0900

    git

--- step.sh
+++ step.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-BASE_COMMIT=git show-branch --sha1-name master | tail -1 | awk '{print substr($0, index($0, "[") +1, index($0, "]") -2)}'
+BASE_COMMIT= git show-branch --sha1-name master | tail -1 | awk '{print substr($0, index($0, "[") +1, index($0, "]") -2)}'
 #デバッグ文
 echo $BASE_COMMIT
 echo ./node_modules/.bin/cloc --diff --vcs=git $BASE_COMMIT HEAD
