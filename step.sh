9f8eececeb33c39a683c58060a41419667c95c0b
nainaistar <nainaistar@gmail.com>
2019-01-12 23:45:00 +0900

    test

--- step.sh
+++ step.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 差分比較元のハッシュ
-readonly BASE_COMMIT= git show-branch --sha1-name master | tail -1 | awk '{print substr($0, index($0, "[") +1, index($0, "]") -2)}'
+set BASE_COMMIT= git show-branch --sha1-name master | tail -1 | awk '{print substr($0, index($0, "[") +1, index($0, "]") -2)}'
 #デバッグ文
 echo $BASE_COMMIT
 echo ./node_modules/.bin/cloc --diff --vcs=git $BASE_COMMIT HEAD
