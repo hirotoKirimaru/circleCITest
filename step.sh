946bff38d4f5623051e1efe191fdb6871d788229
nainaistar <nainaistar@gmail.com>
2019-01-13 00:56:59 +0900

    shell delete

--- step.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-# 差分比較元のハッシュ
-BASE_COMMIT=$(git show-branch --sha1-name master | tail -1 | awk '{print substr($0, index($0, "[") +1, index($0, "]") -2)}')
-
-# デバッグ
-echo ./node_modules/.bin/cloc --diff --vcs=git $BASE_COMMIT HEAD
-# 差分
-./node_modules/.bin/cloc --diff --vcs=git $BASE_COMMIT HEAD
